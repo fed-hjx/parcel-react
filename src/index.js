cbdb9dd8343736332e24103156b9a1d7fef74c8f
fed-hjx <523886060@qq.com>
2018-01-30 16:42:53 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+class Test extends Component {
+    render() {
+        return (
+            <div>
+                321321321321
+            </div>
+        )
+    }
+}
+ReactDOM.render(<Test></Test>,document.getElementById('app'))
